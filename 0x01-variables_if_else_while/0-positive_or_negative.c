2edfc97b4350e16e7cbdb648e13818e857471151
dreamblyss <iwekuba.henry@gmail.com>
2021-10-20 10:49:12 -0700

    betty correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,27 @@
 
 /**
  * main -entry point
+ *
  * program shows if output is positive, negative or zero
- * return: Always 0 (Success)
- */
+ * 
+ * Return: Always 0 (Success)
 
-int main(void){
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* Code */
-if(n > 0){
-printf("%d is positive \n", n);
-}
-else if(n == 0)
+ */
+int main(void)
 {
-printf("%d is zero \n", n );
-}
-else {
-printf("%d is negative \n", n);
-}
-return 0;
+  
+         int n;
+
+	 srand(time(0));
+         n = rand() - RAND_MAX / 2;
+
+	 
+         if(n > 0)
+                printf("%d is positive \n", n);
+         else if(n == 0)
+                printf("%d is zero \n", n );
+         else 
+                printf("%d is negative \n", n);
+
+         return 0;
 }
