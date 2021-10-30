e6fe564d17389ca5210d02415f1a0bf97bcf784f
dreamblyss <iwekuba.henry@gmail.com>
2021-10-30 15:51:03 -0700

    print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+
+int main (void)
+{
+  int digits;
+  char alphabets;
+
+  for(digits = '0'; digits <= '9'; ++digits)
+  {
+    putchar(digits);
+  }
+
+  for(alphabets = 'a'; alphabets <= 'f'; ++alphabets)
+  {
+    putchar(alphabets);
+  }  
+
+    putchar(0xA);
+    return(0);
+	  
+}
