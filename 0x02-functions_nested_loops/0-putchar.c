a488f13eecca28b7bc8f68b33251f959a6e1b6ed
dreamblyss <iwekuba.henry@gmail.com>
2021-11-03 15:17:14 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+  char *xy = "_putchar";
+
+  while (*xy)
+  {
+
+    _putchar(*xy);
+    xy++;
+    
+  }
+
+  _putchar('\n');
+
+
+  return (0);
+  
+}  
