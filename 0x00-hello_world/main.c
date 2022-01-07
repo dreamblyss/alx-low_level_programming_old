83915dff3c3cdbea250dbe803cfb4507f3bf162f
dreamblyss <iwekuba.henry@gmail.com>
2022-01-07 10:56:45 -0800

    0 preprocessor corr

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,18 +1,13 @@
-
 #include <stdio.h>
 
 /**
-
-main - Entry point
-
-Return: Always 0 (Success)
-
-*/
-
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-
-  printf("0-preprocessor project");
 
   return (0);
 
