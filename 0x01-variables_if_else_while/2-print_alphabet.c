5b5b56c51448b7e60ae87350c167ca9c98bb59d3
dreamblyss <iwekuba.henry@gmail.com>
2022-01-15 11:16:19 -0800

    2 print alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
 /**
-
-* main - Entry point
-
-* 
-
-* Return: Always 0 (Success)
-
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
+char lowcase;
+char newline = 0xA;
 
-        char lowcase;
-        char newline = 0xA;
-
-	for(lowcase = 'a'; lowcase <= 'z'; lowcase++)
-	{
-	  putchar(lowcase);
-      
-	}
-
-	putchar(newline);
-	
-        return(0);
-
+for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+{
+putchar(lowcase);
+}
+putchar(newline);
+return(0);
 }
