8a960d61c36efd405292e0cd21197ebb6dd2f79d
dreamblyss <iwekuba.henry@gmail.com>
2022-01-15 11:41:49 -0800

    4 print alpha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+char lowercase;
+char newlyne = 0xA;
 
-  char lowercase;
-  char newlyne = 0xA;
-
-  for(lowercase = 'a'; lowercase <= 'z'; lowercase++)
-  {
-    if(lowercase == 'e' || lowercase == 'q')
-    {
-      continue;
-    }
-
-    putchar(lowercase);
-  }
-  
-  putchar(newlyne);
-
-  return(0);
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+if (lowercase == 'e' || lowercase == 'q')
+{
+continue;
+}
+putchar(lowercase);
+}
+putchar(newlyne);
+return (0);
 }
