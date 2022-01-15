cfeb5e3193dcd08159db680085df8e3c1ff3484e
dreamblyss <iwekuba.henry@gmail.com>
2022-01-15 11:30:10 -0800

    3 print alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,35 +3,24 @@
 #include <time.h>
 
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+char lowercase;
+char uppercase;
+char newlyne = 0xA;
 
-  char lowercase;
-  char uppercase;
-  char newlyne = 0xA;
-
-  for(lowercase = 'a'; lowercase <= 'z'; lowercase++)
-  {
-      putchar(lowercase);
-  }
-    
-  for(uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-  {
-    putchar(uppercase);
-  }
-
-    putchar(newlyne);
-
-    return (0);
-
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
+putchar(uppercase);
+}
+putchar(newlyne);
+return (0);
 }
