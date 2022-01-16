400ed9f2ac17198047bc1a54cda724ef29e21117
dreamblyss <iwekuba.henry@gmail.com>
2022-01-16 10:41:03 -0800

    7 print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,32 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
-
 int main(void)
 {
+char lowcase;
 
-  char lowcase;
-
-  for(lowcase = 'z'; lowcase >= 'a'; --lowcase)
-  {
-
-    putchar(lowcase);
-
-  }  
-
-  putchar(0xA);
-
-  return(0);
-
+for (lowcase = 'z'; lowcase >= 'a'; --lowcase)
+{
+putchar(lowcase);
+}
+putchar(0xA);
+return (0);
 }
