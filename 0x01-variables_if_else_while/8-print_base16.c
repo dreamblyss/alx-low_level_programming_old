22963ba6bb90f95299c19d2da3fd75af44dbcc0d
dreamblyss <iwekuba.henry@gmail.com>
2022-01-16 10:47:43 -0800

    8 print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
-
 int main (void)
 {
-  int digits;
-  char alphabets;
+int digits;
+char alphabets;
 
-  for(digits = '0'; digits <= '9'; ++digits)
-  {
-    putchar(digits);
-  }
-
-  for(alphabets = 'a'; alphabets <= 'f'; ++alphabets)
-  {
-    putchar(alphabets);
-  }  
-
-    putchar(0xA);
-    return(0);
-	  
+for (digits = '0'; digits <= '9'; ++digits)
+{
+putchar(digits);
+}
+for (alphabets = 'a'; alphabets <= 'f'; ++alphabets)
+{
+putchar(alphabets);
+}  
+putchar(0xA);
+return (0);
 }
