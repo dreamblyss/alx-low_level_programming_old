b3bfcda10d51f1b50b773da6b1ac7eb4769f3065
dreamblyss <iwekuba.henry@gmail.com>
2022-01-16 11:04:44 -0800

    9 print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,43 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-
- *   main - Entry point
-
-
- *
-
-
- * Return: Always 0 (Success)
-
-
- */
-
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+int num;
 
-    int num;
-
-    for(num = '0'; num <= '9'; ++num)
-    {
-
-      putchar(num);
-
-      if(num != '9')  
-      {
-        putchar(',');
-	putchar(' ');
-
-      }	
-       
-      
-    }  
-
-    putchar(0xA);
-
-    return (0);
-}     
+for (num = '0'; num <= '9'; ++num)
+{
+putchar(num);
+if (num != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar(0xA);
+return (0);
+}    
 
 
