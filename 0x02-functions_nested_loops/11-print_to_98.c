26c1cee49bc48ae23c844e97601c2ac8e4a0092f
dreamblyss <iwekuba.henry@gmail.com>
2022-01-18 12:49:39 -0800

    11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,23 +4,43 @@
  * print_to_98 - a function that prints all natural numbers from n to 98
  * user input's number prints to 98, regardless < 98 or > 98
  * @num: number input
- * @n: counter variable
+ * @x: counter variable
+ * @y: counter variable
  * Return: Always 0 (Success)
  */
 void print_to_98(int num)
 {
+int x, y;  
 if (num <= 98)
 {
-for (int n = num; n <= 98; n++)
+for (x = num; x <= 98; x++)
 {
-if (n != 98)
+if (x != 98)
 {
-printf("%d, ", n);
+printf("%d, ", x);
 }
-else if (n == 98)
+else if (x == 98)
 {
-prinf("%d\n", n);
+printf("%d\n", x);
 }
 }
-} 
+}
+ 
+else if (num > 98)
+{
+for (y = num; y >= 98; y--)
+{
+if (y != 98)
+{
+printf("%d, ", y);
+}
+else if (y == 98)
+{
+printf("%d\n", y);
+}
+ 
+}
+ 
+}
+
 }
