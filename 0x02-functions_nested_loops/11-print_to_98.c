59a663a5882a77700ba2cf3b11d2eb9b50854f21
dreamblyss <iwekuba.henry@gmail.com>
2022-01-18 12:57:29 -0800

    11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@
  */
 void print_to_98(int num)
 {
-int x, y;  
+int x, y; 
 if (num <= 98)
 {
 for (x = num; x <= 98; x++)
@@ -25,7 +25,6 @@ printf("%d\n", x);
 }
 }
 }
- 
 else if (num > 98)
 {
 for (y = num; y >= 98; y--)
@@ -37,10 +36,7 @@ printf("%d, ", y);
 else if (y == 98)
 {
 printf("%d\n", y);
+} 
 }
- 
 }
- 
-}
-
 }
