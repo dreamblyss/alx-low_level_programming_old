e7ef6954f5e3e705b8c18f4c1931f46a1f738865
dreamblyss <iwekuba.henry@gmail.com>
2022-01-20 13:08:05 -0800

    5 more numbers corr

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@
 void more_numbers(void)
 {
 int l1, l2;
-for(l1 = 1; l1 <= 10; l1++)
+for (l1 = 1; l1 <= 10; l1++)
 {
-for(l2 = 0; l2 <= 14; l2++)
+for (l2 = 0; l2 <= 14; l2++)
 {
-if(l2 >= 10)
+if (l2 >= 10)
 {
-_putchar(( l2 / 10) + '0');
+_putchar((l2 / 10) + '0');
 }
 _putchar((l2 % 10) + '0');
 }
