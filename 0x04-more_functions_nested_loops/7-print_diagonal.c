9c7951515d8ab79894b1dd5008c3a7b93c1c82d3
dreamblyss <iwekuba.henry@gmail.com>
2022-01-28 06:14:24 -0800

    7 print diag corr

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
-* print diagonal - a function that draws a diagonal line on the terminal
-* @n: input number of times '\' should be printed
-* Return: a diagonal
-*/
+ * print diagonal - a function that draws a diagonal line on the terminal
+ * @n: input number of times '\' should be printed
+ * Return: a diagonal
+ */
 void print_diagonal(int n)
 {
 int k, l;
